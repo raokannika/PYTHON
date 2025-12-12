fa00b3d3f18cda89c3e6f735ba3f8f91a461273c
kannika07112 <kkannikarao07@gmail.com>
2025-12-12 10:36:36 +0530

    simple

---
+++ machine_learning/program8_eda.py
@@ -0,0 +1,7 @@
+# Program 5: Basic EDA
+import pandas as pd
+
+df = pd.read_csv("titanic.csv")  # Use a Titanic dataset CSV
+print(df.head())
+print("Survival rate:", df["Survived"].mean())
+print("Age Stats:\n", df["Age"].describe())
